7d2821dc4d82257c1cc86f3e679db1f215b2d49c
kishyassin <kishyassin@gmail.com>
2024-11-23 15:03:17 +0100

    UPDATE README.md

---
+++ app/Filament/Resources/ConfigurationResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ConfigurationResource\Pages;
+use App\Filament\Resources\ConfigurationResource\RelationManagers;
+use App\Models\Configuration;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ConfigurationResource extends Resource
+{
+    protected static ?string $model = Configuration::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('Default Title'),
+                Forms\Components\TextInput::make('mail')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('default@mail.com'),
+                Forms\Components\TextInput::make('addresse')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('Default Address'),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255)
+                    ->default('000-000-0000'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('mail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('addresse')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListConfigurations::route('/'),
+            'create' => Pages\CreateConfiguration::route('/create'),
+            'edit' => Pages\EditConfiguration::route('/{record}/edit'),
+        ];
+    }
+}
