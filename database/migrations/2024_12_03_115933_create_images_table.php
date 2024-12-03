55bc8ca6a68329e4a3473609baff6e1d81394ea9
kishyassin <kishyassin@gmail.com>
2024-12-04 00:01:31 +0100

    image upload please change APP URL in .env to http://127.0.0.1:8000

---
+++ database/migrations/2024_12_03_115933_create_images_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('heroImage');
+            $table->string('squareImage');
+            $table->string('panoramaImage');
+            $table->string('largeHeroImage');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
